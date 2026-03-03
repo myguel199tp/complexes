6332ee9efb622112786c363bddbafd086fd5e73d
myguel199tp <jmtiradop19@gmail.com>
2026-03-03 14:40:10 -0500

    feat: conocer

--- app/(dashboard)/complexes/_components/hompage.tsx
+++ app/(dashboard)/complexes/_components/hompage.tsx
@@ -125,7 +125,7 @@ export default function Homepage() {
             </Text>
 
             <Button colVariant="primary" rounded="lg" className="mt-6">
-              Conocer más
+              Conocer
             </Button>
           </div>
 
